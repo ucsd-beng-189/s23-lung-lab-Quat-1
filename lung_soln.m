5a9088a4d66abbb4d8091c623e0baad773c01809
Jerry Qiao <jerryluqiao@gmail.com>
2023-04-22 17:04:33 -0700

    Task 6
    -  Commented out Task 5 code in lung_soln.m
    - Modified setup_lung_soln.m to run Task 6

--- lung_soln.m
+++ lung_soln.m
@@ -3,19 +3,62 @@ clear all
 clf
 global Pstar cstar n maxcount M Q camax RT cI;
 
-% Task 5
-cIRange = linspace(0.0079, 0, 11); % Range of cI values, decreasing to 0
+% % Task 5
+% cIRange = linspace(0.0079, 0, 11); % Range of cI values, decreasing to 0
+% 
+% PAbarRange = zeros(1,length(cIRange)); % Mean alveolar partial pressure of oxygen
+% PabarRange = zeros(1,length(cIRange)); % Mean arterial partial pressure of oxygen
+% PvRange = zeros(1,length(cIRange)); % Venous partial pressure of oxygen
+% 
+% cAbarRange = zeros(1,length(cIRange)); % Mean alveolar oxygen concentration
+% cabarRange = zeros(1,length(cIRange)); % Mean arterial oxygen concentration
+% cvRange = zeros(1,length(cIRange)); % Oxygen concentration in venous blood
+% 
+% for i = 1:length(cIRange)
+%     cI = cIRange(i);
+%     setup_lung_soln
+%     cvsolve_soln
+%     outchecklung
+% 
+%     PAbarRange(i) = PAbar; 
+%     PabarRange(i) = Pabar;
+%     PvRange(i) = Pv;
+% 
+%     cAbarRange(i) = cAbar;
+%     cabarRange(i) = cabar;
+%     cvRange(i) = cv;
+% end
+% 
+% figure(4)
+% subplot(2,1,1)
+% hold on
+% plot(cIRange, PAbarRange)
+% plot(cIRange, PabarRange)
+% plot(cIRange, PvRange)
+% title('Partial Pressure of Oxygen vs Inspired Concentration of Oxygen')
+% xlabel('cI')
+% ylabel('Partial Pressure of Oxygen')
+% subplot(2,1,2)
+% hold on
+% plot(cIRange, cAbarRange)
+% plot(cIRange, cabarRange)
+% plot(cIRange, cvRange)
+% title('Concentration of Oxygen vs Inspired Concentration of Oxygen')
+% xlabel('cI')
+% ylabel('Concentration of Oxygen')
 
-PAbarRange = zeros(1,length(cIRange)); % Mean alveolar partial pressure of oxygen
-PabarRange = zeros(1,length(cIRange)); % Mean arterial partial pressure of oxygen
-PvRange = zeros(1,length(cIRange)); % Venous partial pressure of oxygen
+% Task 6
+PIRange = linspace(152,0,11);
+PAbarRange = zeros(1,length(PIRange)); % Mean alveolar partial pressure of oxygen
+PabarRange = zeros(1,length(PIRange)); % Mean arterial partial pressure of oxygen
+PvRange = zeros(1,length(PIRange)); % Venous partial pressure of oxygen
 
-cAbarRange = zeros(1,length(cIRange)); % Mean alveolar oxygen concentration
-cabarRange = zeros(1,length(cIRange)); % Mean arterial oxygen concentration
-cvRange = zeros(1,length(cIRange)); % Oxygen concentration in venous blood
+cAbarRange = zeros(1,length(PIRange)); % Mean alveolar oxygen concentration
+cabarRange = zeros(1,length(PIRange)); % Mean arterial oxygen concentration
+cvRange = zeros(1,length(PIRange)); % Oxygen concentration in venous blood
 
-for i = 1:length(cIRange)
-    cI = cIRange(i);
+for i = 1:length(PIRange)
+    PI = PIRange(i);
     setup_lung_soln
     cvsolve_soln
     outchecklung
@@ -32,17 +75,19 @@ end
 figure(4)
 subplot(2,1,1)
 hold on
-plot(cIRange, PAbarRange)
-plot(cIRange, PabarRange)
-plot(cIRange, PvRange)
-title('Partial Pressure of Oxygen vs Inspired Concentration of Oxygen')
-xlabel('cI')
+plot(PIRange, PAbarRange)
+plot(PIRange, PabarRange)
+plot(PIRange, PvRange)
+title('Partial Pressure of Oxygen vs Inspired Partial Pressure of Oxygen')
+xlabel('PI')
 ylabel('Partial Pressure of Oxygen')
+legend('PABar', 'Pabar', 'Pv')
 subplot(2,1,2)
 hold on
-plot(cIRange, cAbarRange)
-plot(cIRange, cabarRange)
-plot(cIRange, cvRange)
-title('Concentration of Oxygen vs Inspired Concentration of Oxygen')
-xlabel('cI')
+plot(PIRange, cAbarRange)
+plot(PIRange, cabarRange)
+plot(PIRange, cvRange)
+title('Concentration of Oxygen vs Inspired Partial Pressure of Oxygen')
+xlabel('PI')
 ylabel('Concentration of Oxygen')
+legend('cAbar', 'cabar', 'cv')
