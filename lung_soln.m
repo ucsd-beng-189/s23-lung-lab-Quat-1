a28f8f38601c76a55f27dfe80f89039a412b2e32
Jerry Qiao <jerryluqiao@gmail.com>
2023-04-22 18:21:49 -0700

    Task 9
    - Commented out the code for Task 8 in lung.soln.m, and added in code for Task 9
    - Changed around the values of Beta
    - Added in plots for Task 9

--- lung_soln.m
+++ lung_soln.m
@@ -49,26 +49,76 @@ global Pstar cstar n maxcount M Q camax RT cI;
 % ylabel('Concentration of Oxygen')
 % legend('cAbar', 'cabar', 'cv')
 
-% Task 6, Task 7, Task 8
-PIRange = linspace(152,0,11);
-PAbarRange = zeros(1,length(PIRange)); % Mean alveolar partial pressure of oxygen
-PabarRange = zeros(1,length(PIRange)); % Mean arterial partial pressure of oxygen
-PvRange = zeros(1,length(PIRange)); % Venous partial pressure of oxygen
+% % Task 6, Task 7, Task 8
+% PIRange = linspace(152,0,11);
+% PAbarRange = zeros(1,length(PIRange)); % Mean alveolar partial pressure of oxygen
+% PabarRange = zeros(1,length(PIRange)); % Mean arterial partial pressure of oxygen
+% PvRange = zeros(1,length(PIRange)); % Venous partial pressure of oxygen
+% 
+% cAbarRange = zeros(1,length(PIRange)); % Mean alveolar oxygen concentration
+% cabarRange = zeros(1,length(PIRange)); % Mean arterial oxygen concentration
+% cvRange = zeros(1,length(PIRange)); % Oxygen concentration in venous blood
+% 
+% for i = 1:length(PIRange)
+%     PI = PIRange(i);
+%     setup_lung_soln
+%     cvsolve_soln
+%     outchecklung
+% 
+%     PAbarRange(i) = PAbar; 
+%     PabarRange(i) = Pabar;
+%     PvRange(i) = Pv;
+% 
+%     cAbarRange(i) = cAbar;
+%     cabarRange(i) = cabar;
+%     cvRange(i) = cv;
+% end
+% 
+% figure(4)
+% subplot(2,1,1)
+% hold on
+% plot(PIRange, PAbarRange)
+% plot(PIRange, PabarRange)
+% plot(PIRange, PvRange)
+% title('Partial Pressure of Oxygen vs Inspired Partial Pressure of Oxygen')
+% xlabel('PI')
+% ylabel('Partial Pressure of Oxygen')
+% legend('PABar', 'Pabar', 'Pv')
+% subplot(2,1,2)
+% hold on
+% plot(PIRange, cAbarRange)
+% plot(PIRange, cabarRange)
+% plot(PIRange, cvRange)
+% title('Concentration of Oxygen vs Inspired Partial Pressure of Oxygen')
+% xlabel('PI')
+% ylabel('Concentration of Oxygen')
+% legend('cAbar', 'cabar', 'cv')
 
-cAbarRange = zeros(1,length(PIRange)); % Mean alveolar oxygen concentration
-cabarRange = zeros(1,length(PIRange)); % Mean arterial oxygen concentration
-cvRange = zeros(1,length(PIRange)); % Oxygen concentration in venous blood
+% Task 9
+cstarRange = linspace(0.0079, 0, 11); % Range of cstar values, from default to 0.
 
-for i = 1:length(PIRange)
-    PI = PIRange(i);
+PIRange = zeros(1,length(cstarRange)); % Inspired partial pressure of oxygen
+PAbarRange = zeros(1,length(cstarRange)); % Mean alveolar partial pressure of oxygen
+PabarRange = zeros(1,length(cstarRange)); % Mean arterial partial pressure of oxygen
+PvRange = zeros(1,length(cstarRange)); % Venous partial pressure of oxygen
+
+cIRange = zeros(1,length(cstarRange)); % Inspired concentration of oxygen
+cAbarRange = zeros(1,length(cstarRange)); % Mean alveolar oxygen concentration
+cabarRange = zeros(1,length(cstarRange)); % Mean arterial oxygen concentration
+cvRange = zeros(1,length(cstarRange)); % Oxygen concentration in venous blood
+
+for i = 1:length(cstarRange)
+    cstar = cstarRange(i);
     setup_lung_soln
     cvsolve_soln
     outchecklung
 
+    PIRange(i) = PI;
     PAbarRange(i) = PAbar; 
     PabarRange(i) = Pabar;
     PvRange(i) = Pv;
 
+    cIRange(i) = cI;
     cAbarRange(i) = cAbar;
     cabarRange(i) = cabar;
     cvRange(i) = cv;
@@ -77,19 +127,21 @@ end
 figure(4)
 subplot(2,1,1)
 hold on
-plot(PIRange, PAbarRange)
-plot(PIRange, PabarRange)
-plot(PIRange, PvRange)
-title('Partial Pressure of Oxygen vs Inspired Partial Pressure of Oxygen')
-xlabel('PI')
+plot(cstarRange, PIRange)
+plot(cstarRange, PAbarRange)
+plot(cstarRange, PabarRange)
+plot(cstarRange, PvRange)
+title('Partial Pressure of Oxygen vs Hemoglobin Concentration')
+xlabel('cstar (mol/L)')
 ylabel('Partial Pressure of Oxygen')
-legend('PABar', 'Pabar', 'Pv')
+legend('PI', 'PABar', 'Pabar', 'Pv')
 subplot(2,1,2)
 hold on
-plot(PIRange, cAbarRange)
-plot(PIRange, cabarRange)
-plot(PIRange, cvRange)
-title('Concentration of Oxygen vs Inspired Partial Pressure of Oxygen')
-xlabel('PI')
+plot(cstarRange, cIRange)
+plot(cstarRange, cAbarRange)
+plot(cstarRange, cabarRange)
+plot(cstarRange, cvRange)
+title('Concentration of Oxygen vs Hemoglobin Concentration')
+xlabel('cstar (mol/L)')
 ylabel('Concentration of Oxygen')
-legend('cAbar', 'cabar', 'cv')
+legend('cI', 'cAbar', 'cabar', 'cv')
